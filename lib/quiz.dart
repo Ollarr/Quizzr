68a17d0afc872c1a8fba5e4c8166d5135812db17
Oluwasegun <oluyedis@gmail.com>
2023-06-23 19:45:24 +0100

    implemented the state change functionality to show a different screen using ternary expression to conditionally render pages

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import "package:quizzr/home_screen.dart";
+import 'package:quizzr/questions._screen.dart';
 
 class QuizScreen extends StatefulWidget {
   const QuizScreen({super.key});
@@ -11,10 +12,49 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
+  // Note: it's possible to assign a widget to a variable. e.g var activeScreen = HomeScreen();
+  // var activeScreen = "start-screen";
+// This is one way of initializing a variable and later assigning a value to it, in our case the value assigned in the iniState function is a Widget.
+  // Widget? activeScreen;
+  // @override
+  // void initState() {
+  //   activeScreen = HomeScreen(switchScreen);
+
+  //   super.initState();
+  // }
+
+// To do this without using the initState function, one can use a ternary expression.
+  var activeScreen = "home-screen";
+
+  void switchScreen() {
+    setState(() {
+      // activeScreen = const QuestionsScreen();
+      activeScreen = "question-screen";
+    });
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Quizzr"), centerTitle: true),
-        body: const HomeScreen());
+      appBar: AppBar(title: const Text("Quizzr"), centerTitle: true),
+      body: Container(
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(colors: [
+              Color.fromARGB(255, 4, 91, 162),
+              Color.fromARGB(255, 19, 172, 108)
+            ]),
+            image: DecorationImage(
+              image: const AssetImage("assets/images/quiz-logo.png"),
+              fit: BoxFit.contain,
+              colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.7),
+                BlendMode.dstOut,
+              ),
+            ),
+          ),
+          child: activeScreen == "home-screen"
+              ? HomeScreen(switchScreen)
+              : const QuestionsScreen()),
+    );
   }
 }

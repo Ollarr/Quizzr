08abe6595c6fab5713ef5c407f81af3f9fec26b1
Oluwasegun <oluyedis@gmail.com>
2023-06-23 19:47:14 +0100

    create a class to model how the question data should look

---
+++ lib/models/quiz_question_model.dart
@@ -0,0 +1,7 @@
+// This is a blueprint of how the questions data should look like
+class QuizQuestion {
+  const QuizQuestion(this.text, this.answers);
+
+  final String text;
+  final List<String> answers;
+}

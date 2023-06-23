0ac16393ad5431b9e0d1815d2047d09db9fac297
Oluwasegun <oluyedis@gmail.com>
2023-06-23 19:42:14 +0100

    refactored the home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,32 +1,19 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
   @override
   Widget build(context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: const LinearGradient(colors: [
-          Color.fromARGB(255, 4, 91, 162),
-          Color.fromARGB(255, 19, 172, 108)
-        ]),
-        image: DecorationImage(
-          image: const AssetImage("assets/images/quiz-logo.png"),
-          fit: BoxFit.contain,
-          colorFilter: ColorFilter.mode(
-            Colors.black.withOpacity(0.7),
-            BlendMode.dstOut,
-          ),
-        ),
-      ),
-      child: Center(
-        child: OutlinedButton.icon(
-          onPressed: () {},
-          icon: const Icon(Icons.arrow_right_alt, color: Colors.white),
-          label: const Text(
-            "Start Quiz",
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
+    return Center(
+      child: OutlinedButton.icon(
+        onPressed: startQuiz,
+        icon: const Icon(Icons.arrow_right_alt, color: Colors.white),
+        label: const Text(
+          "Start Quiz",
+          style: TextStyle(color: Colors.white, fontSize: 20),
         ),
       ),
     );

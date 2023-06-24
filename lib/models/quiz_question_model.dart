7f12f172886ec1957b758276954fe81ef147e86a
Oluwasegun <oluyedis@gmail.com>
2023-06-24 19:16:59 +0100

    Added a function to shuffle answers

--- lib/models/quiz_question_model.dart
+++ lib/models/quiz_question_model.dart
@@ -4,4 +4,10 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shuffledAnswers = List.of(answers);
+    shuffledAnswers.shuffle();
+    return shuffledAnswers;
+  }
 }

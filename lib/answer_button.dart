53f070b6b200c0bcb043e6b8ad8535b886ad2a87
Oluwasegun <oluyedis@gmail.com>
2023-07-14 07:02:49 +0100

    Added and styled amwers button.

---
+++ lib/answer_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(
+      {super.key, required this.answerText, required this.onPress});
+  final String answerText;
+  final void Function() onPress;
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 40, 3, 104),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        ),
+        onPressed: onPress,
+        child: Text(
+          answerText,
+          textAlign: TextAlign.center,
+        ));
+  }
+}

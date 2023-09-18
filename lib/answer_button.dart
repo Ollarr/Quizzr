47295f33e3ae9355dcdcd9958387565d0b6cc675
Oluwasegun <oluyedis@gmail.com>
2023-09-18 14:13:43 +0100

    Added a question screen widget and styled it. Created a custom class that serves as a blueprint for questions text, possible answers as well as a utility method that shuffle these answers

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 class AnswerButton extends StatelessWidget {
   const AnswerButton(
       {super.key, required this.answerText, required this.onPress});
+
   final String answerText;
   final void Function() onPress;
+
   @override
   Widget build(context) {
     return ElevatedButton(

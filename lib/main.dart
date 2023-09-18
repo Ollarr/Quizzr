e6c66b5e41289591dec190ccc02b9c4f232d5793
Oluwasegun <oluyedis@gmail.com>
2023-09-18 14:04:41 +0100

    Made some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quizzr/quiz.dart';
+import 'package:quizzr/quiz_screen.dart';
 
+// This main function (in the main.dart file) is the entry point of every app as it is automatically executed by Dart when the app starts
 void main() {
   runApp(const MaterialApp(home: QuizScreen()));
 }

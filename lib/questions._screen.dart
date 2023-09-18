a6cea7a6943dc16ce1067788c246255ade678b6c
Oluwasegun <oluyedis@gmail.com>
2023-09-18 14:13:21 +0100

    Added a question screen widget and styled it. Created a custom class that serves as a blueprint for questions text, possible answers as well as a utility method that shuffle these answers

--- lib/questions._screen.dart
+++ lib/questions._screen.dart
@@ -3,7 +3,12 @@ import 'package:quizzr/answer_button.dart';
 import 'package:quizzr/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  // onSelectedAnswer is a an argument that is accepted here as an arg as a pointer
+  //to make the chooseAnswer function/method available and accesible to the button that triggers it.
+  //N/B Whatever name you give to it is up to you
+  const QuestionsScreen({super.key, required this.onSelectAnswer});
+
+  final void Function(String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() {
@@ -14,18 +19,24 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void changeQuestion() {
+  void answerQuestion(String selectedAnswer) {
+    // N/B: this is done because the onSelectAnswer constructor property only is available in the
+    //QuestionsScreen widget and therefore to make accesible in this widget that extends
+    //the QuestionsScreen, we have use this in-built widget
+    widget.onSelectAnswer(selectedAnswer);
     setState(() {
-      currentQuestionIndex = currentQuestionIndex < questions.length
-          ? currentQuestionIndex++
-          :
-          // TODO: show result page
-          0;
+      currentQuestionIndex++;
+      // currentQuestionIndex = currentQuestionIndex < questions.length
+      //     ? currentQuestionIndex ++ // Increment when less than the length
+      //     : 0; // Reset to 0 when it reaches the length
+
+      //     then show result page
+
       // if (currentQuestionIndex < questions.length) {
       //   currentQuestionIndex++;
       // } else {
       //   currentQuestionIndex = 0;
-      //   // TODO: show result page
+      //  then show result page
       // }
     });
   }
@@ -36,6 +47,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
     return SizedBox(
       width: double.infinity,
+      // N/B COntainers by default, do not take up entire available space on the screen
       child: Container(
         margin: const EdgeInsets.all(40),
         child: Column(
@@ -51,9 +63,19 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               ),
               textAlign: TextAlign.center,
             ),
+            // The SizedBox built-in Widget is mostly used to add space between two items(stacked or placed beside each other)
             const SizedBox(height: 30),
+            // The spread operator is used here because what the children arg expects is a
+            //list of widget but in our case, it is getting list that contains another list,
+            //so what this spread operator essentially does is to pull all the items/values
+            //in a list/iterable and add them as a standalone comma seperated items/value to a parent/surrounding list
             ...currentQuestion.getShuffledAnswers().map((answer) {
-              return AnswerButton(onPress: changeQuestion, answerText: answer);
+              return AnswerButton(
+                  onPress: () {
+                    // The answerQuestion function here takes the answers selected for each of the question as an arg
+                    answerQuestion(answer);
+                  },
+                  answerText: answer);
             })
           ],
         ),
